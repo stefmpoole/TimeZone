14a96f925aa1352942d413df7ae718857af08ba5
stefmpoole <104706141+stefmpoole@users.noreply.github.com>
2022-12-15 18:10:07 -0500

    added function displayTime
    added function displayTime, changed cities, updated div classes in HTML, changed css removed border

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
+function displayTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let displayDateNewYork = newYorkElement.querySelector("#date");
+  let displayTimeNewYork = newYorkElement.querySelector("#time");
+  let newYorkTime = moment().tz("America/New_York");
+
+  displayDateNewYork.innerHTML = newYorkTime.format("dddd, MMMM Do YYYY");
+  displayTimeNewYork.innerHTML = newYorkTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  let londonElement = document.querySelector("#london");
+  let displayDateLondon = londonElement.querySelector("#date");
+  let displayTimeLondon = londonElement.querySelector("#time");
+  let londonTime = moment().tz("Europe/Paris");
+
+  displayDateLondon.innerHTML = londonTime.format("dddd, MMMM Do YYYY");
+  displayTimeLondon.innerHTML = londonTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let displayDatetokyo = tokyoElement.querySelector("#date");
+  let displayTimetokyo = tokyoElement.querySelector("#time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  displayDatetokyo.innerHTML = tokyoTime.format("dddd, MMMM Do YYYY");
+  displayTimetokyo.innerHTML = tokyoTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+}
+
+displayTime();
+setInterval(displayTime, 1);
+
 let citySelect = document.querySelector("#cities");
-let displayDate = document.querySelector("#date");
-let displayTime = document.querySelector("#time");
 
 function showSelectedCity(event) {
   if (event.target.value.length > 0) {
@@ -8,8 +41,8 @@ function showSelectedCity(event) {
     let currentDate = moment()
       .tz(event.target.value)
       .format("dddd, MMMM Do YYYY");
-    displayDate.innerHTML = `${currentDate}`;
-    displayTime.innerHTML = `${currentTime}`;
+    alert(currentDate);
+    alert(currentTime);
   }
 }
 
